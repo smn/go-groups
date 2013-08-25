66768952446dc40d796157ea1cdd06b0279c29a3
Simon de Haan <simon@praekeltfoundation.org>
2013-08-25 11:34:22 +0200

    basic groups dummy api implemented

--- test/test-go-groups.js
+++ test/test-go-groups.js
@@ -6,19 +6,111 @@ var app = require("../lib/go-groups");
 var api = app.api;
 
 api.groups_store = {};
+api.groups_search_store = {};
+
 api.add_group = function(group) {
   this.groups_store[group.key] = group;
   return group;
 };
 
+api.add_group_search = function(query, groups) {
+  this.group_search_store[query] = groups;
+};
+
 api._handle_groups_list = function(cmd, reply) {
   var groups_store = this.groups_store;
-  reply(api._populate_reply(cmd, {
+  api._reply_success(cmd, reply, {
     groups: Object.keys(groups_store).map(function(key) {
       return groups_store[key];
     })
-  }));
+  });
 };
+
+api._handle_groups_search = function(cmd, reply) {
+  var groups_store = this.groups_store;
+  api._reply_success(cmd, reply, {
+    groups: api.group_search_store[cmd.query] || []
+  });
+};
+
+api._handle_groups_get = function(cmd, reply) {
+  var group = this.groups_store[cmd.key];
+  if(group) {
+    api._reply_success(cmd, reply, {
+      group: group
+    });
+  } else {
+    api._reply_fail(cmd, reply, 'Group not found');
+  }
+};
+
+api._handle_groups_get_by_name = function(cmd, reply) {
+  matches = this.groups_store.filter(function(group) {
+    return group.name == cmd.name;
+  });
+  if(matches.length === 0) {
+    api._reply_fail(cmd, reply, 'Group not found');
+  } else if (matches.length > 1) {
+    api._reply_fail(cmd, reply, 'More than one group returned');
+  } else {
+    api._reply_success(cmd, reply, {group: matches[0]});
+  }
+};
+
+api._handle_groups_get_or_create_by_name = function(cmd, reply) {
+  matches = this.groups_store.filter(function(group) {
+    return group.name == cmd.name;
+  });
+  if(matches.length === 0) {
+    api._reply_success(cmd, reply, {
+      group: api.add_group({key: this.generate_key(), name: cmd.name})
+    });
+  } else if (matches.length > 1) {
+    api._reply_fail(cmd, reply, 'More than one group returned');
+  } else {
+    api._reply_success(cmd, reply, {group: matches[0]});
+  }
+};
+
+api._handle_groups_update = function(cmd, reply) {
+  group = this.groups_store[cmd.key];
+  if(!group) {
+    api._reply_fail(cmd, reply, 'Group not found');
+  } else {
+    api._reply_success(cmd, reply, {
+      group: this.add_group({
+        key: group.key,
+        name: cmd.name,
+        query: cmd.query
+      })
+    });
+  }
+};
+
+api._handle_groups_count_members = function(cmd, reply) {
+  var group = api.groups_store[cmd.key];
+  if(!group) {
+    api._reply_fail(cmd, reply, 'Group not found');
+  } else {
+    var is_smart_group = (cmd.query ? true : false);
+    var member_count;
+    if(is_smart_group) {
+      // TODO:  not sure what to do about this one as I don't feel like
+      //        implementing the Lucene query syntax.
+      member_count = 42;
+    } else {
+      var contacts = this.contact_store.filter(function(contact) {
+        return contact.groups.indexOf(group.key) > -1;
+      });
+      member_count = contacts.length;
+    }
+    api._reply_success(cmd, reply, {
+      group: group,
+      count: member_count
+    });
+  }
+};
+
 
 describe('Go Contacts', function () {
 
