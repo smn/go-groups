39a42a895f2800216c2cd6283ccb0773392ec6c9
Simon de Haan <simon@praekeltfoundation.org>
2013-08-25 09:55:32 +0200

    small refactor

--- test/test-go-groups.js
+++ test/test-go-groups.js
@@ -3,32 +3,35 @@ var assert = require("assert");
 var vumigo = require("vumigo_v01");
 var app = require("../lib/go-groups");
 
+var api = app.api;
+
+api.groups_store = {};
+api.add_group = function(group) {
+  this.groups_store[group.key] = group;
+  return group;
+};
+
+api._handle_groups_list = function(cmd, reply) {
+  var groups_store = this.groups_store;
+  reply(api._populate_reply(cmd, {
+    groups: Object.keys(groups_store).map(function(key) {
+      return groups_store[key];
+    })
+  }));
+};
+
 describe('Go Contacts', function () {
 
   var tester;
-  var api = app.api;
 
   beforeEach(function () {
-
-    tester = new vumigo.test_utils.ImTester(api, {
+    tester = new vumigo.test_utils.ImTester(app.api, {
       async: true
     });
 
-
-    api._handle_groups_list = function(cmd, reply) {
-      reply(api._populate_reply(cmd, {
-        groups: [{
-          key: 'group-1',
-          name: 'Group 1'
-        }, {
-          key: 'group-2',
-          name: 'Group 2'
-        }, {
-          key: 'group-3',
-          name: 'Group 3'
-        }]
-      }));
-    };
+    api.add_group({ key: 'group-1', name: 'Group 1'});
+    api.add_group({ key: 'group-2', name: 'Group 2'});
+    api.add_group({ key: 'group-3', name: 'Group 3'});
   });
 
   it('should list known groups', function(done) {
